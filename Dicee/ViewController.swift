7d7658d35e68bb7dfb9a49a09e560c7a7976874e
Pranoy Dev <devpranoy@gmail.com>
2017-12-10 00:07:21 +0530

    Function Implementation

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,11 +27,14 @@ class ViewController: UIViewController {
 
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+        
+    }
+    func updateDiceImages(){
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         diceImageView1.image = UIImage(named:diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named:diceArray[randomDiceIndex2])
-        
     }
 }
 
